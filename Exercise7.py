0cb3448b87a7e4cb5019c5ac52adda5c6adc43f0
Manali Deb <Debook4@gmail.com>
2026-06-17 13:13:28 -0500

    added exercise 7

---
+++ Exercise7.py
@@ -0,0 +1,16 @@
+# Exercise 7
+"""
+FizzBuzz Function
+
+Write a function called `fizzBuzz(n)` that returns:
+- `"FizzBuzz"` if n is divisible by both 3 and 5
+- `"Fizz"` if n is divisible by 3 only
+- `"Buzz"` if n is divisible by 5 only
+- The number itself (as a string) if none of the above
+
+Then write `printFizzBuzz(start, end)` that calls `fizzBuzz()` for every number from start to end and prints the result.
+
+Call `printFizzBuzz(1, 30)`.
+
+Hint: look up the `%` (modulo) operator
+"""
