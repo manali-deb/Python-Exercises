3406d00f247bfc61559b83d2c98652f5d04bf42f
Manali Deb <Debook4@gmail.com>
2026-06-17 15:14:45 -0500

    added 13

---
+++ Exercise13.py
@@ -0,0 +1,20 @@
+# Exercise 13
+"""
+Rock Paper Scissors
+
+Write a function `getResult(player, computer)` where both values are `"rock"`, `"paper"`, or `"scissors"`. Return:
+- `"You win!"` if the player beats the computer
+- `"Computer wins!"` if the computer wins
+- `"It's a tie!"` if they're equal
+- `"Invalid move"` if either value is not one of the three options
+
+Then write `printGame(player, computer)` that prints:
+```
+Player:   rock
+Computer: scissors
+Result:   You win!
+```
+
+Test all possible combinations (there are 9).
+"""
+
