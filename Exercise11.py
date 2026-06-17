d4362603bb2b851341e9e422151a666255ffcc57
Manali Deb <Debook4@gmail.com>
2026-06-17 14:41:19 -0500

    finished 11 + tests

--- Exercise11.py
+++ Exercise11.py
@@ -16,3 +16,47 @@ Test with: `[4, 8, 15, 16, 23, 42]`, `[100, 200, 50, 75, 125]`, `[3]`
 
 Do NOT use Python's built-in `sum()`, `min()`, or `max()` — write the logic yourself using a loop.
 """
+def getStats(numbers):
+    count = len(numbers)
+
+    total = 0
+    minimum = numbers[0]
+    maximum = numbers[0]
+
+    for num in numbers:
+        total += num
+        if num > maximum:
+            maximum = num
+        if num < minimum:
+            minimum = num
+
+    average = total / count
+    range = maximum - minimum
+
+    numDict = {
+        "count": count,
+        "total": total,
+        "average": average,
+        "minimum": minimum,
+        "maximum": maximum,
+        "range": range
+    }
+
+    return numDict
+
+def printStats(numbers):
+    numDict = getStats(numbers)
+    print(f"Statistics for {numbers}:")
+    for key, value in numDict.items():
+        print(f"{key}: {value}")
+
+    print()
+
+# test
+set1 = [4, 8, 15, 16, 23, 42]
+set2 = [100, 200, 50, 75, 125]
+set3 = [3]
+
+printStats(set1)
+printStats(set2)
+printStats(set3)
