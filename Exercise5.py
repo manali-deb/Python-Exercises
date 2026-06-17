b57cbba930a713373bb4b4e61f5adf64bf2e3b17
Manali Deb <Debook4@gmail.com>
2026-06-17 13:02:53 -0500

    exercise 5 + tests

--- Exercise5.py
+++ Exercise5.py
@@ -20,3 +20,28 @@ Test with at least 3 different scenarios.
 
 Hint: look up `round()` to keep it to 2 decimal places.
 """
+
+def calculateTip(billAmount, tipPercent):
+    return billAmount * (tipPercent/100)
+
+def splitBill(billAmount, tipPercent, numPeople):
+    tip = calculateTip(billAmount, tipPercent)
+    total = billAmount + tip
+    return total/numPeople
+
+def printBillSummary(billAmount, tipPercent, numPeople):
+    tip = calculateTip(billAmount, tipPercent)
+    total = billAmount + tip
+    eachPerson = splitBill(billAmount, tipPercent, numPeople)
+
+    print(f"Bill:           ${billAmount:.2f}")
+    print(f"Tip ({tipPercent}%):      ${tip:.2f}")
+    print(f"Total:          ${total:.2f}")
+    print(f"People:         {numPeople}")
+    print(f"Each person:    ${eachPerson:.2f}")
+    print()
+
+# tests
+printBillSummary(86.89, 19, 5)
+printBillSummary(40, 10, 2)
+printBillSummary(896, 30, 8)
