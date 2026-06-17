6aa83eef1fb1226c445033aa4ba5f440e0ee9627
Manali Deb <Debook4@gmail.com>
2026-06-17 11:58:47 -0500

    Exercise 2 completed

--- Exercise2.py
+++ Exercise2.py
@@ -16,3 +16,32 @@ Then write a `printCalculation(a, b, operator)` function that prints:
 
 Test with at least 8 different combinations.
 """
+
+def calculate(a, b, operator):
+    if operator == "+":
+        return a + b
+    elif operator == "-":
+        return a - b
+    elif operator == "*":
+        return a * b
+    elif operator == "/":
+        if b == 0:
+            return "Cannot divide by zero"
+        return a / b
+    else:
+        return "Invalid operator"
+    
+def printCalculation(a, b, operator):
+    result = calculate(a, b, operator)
+    print(f"{a} {operator} {b} = {result}")
+
+# tests
+printCalculation(10, 5, "+")
+printCalculation(10, 0, "/")
+printCalculation(8, 6, "+")
+printCalculation(10, 15, "-") # negative numbers test
+printCalculation(300, 84, "-")
+printCalculation(18, 35, "*")
+printCalculation(9, 8, "*")
+printCalculation(95, 5, "/")
+printCalculation(34, 6, "/") #decimal test
