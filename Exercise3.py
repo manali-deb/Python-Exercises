1cbdc41a2b13747761350eb9518576f02762fd18
Manali Deb <Debook4@gmail.com>
2026-06-17 12:20:09 -0500

    Exercise 3 added

--- Exercise3.py
+++ Exercise3.py
@@ -17,3 +17,31 @@ Manali scored 85/100 and received a B
 
 Test with scores: `105`, `95`, `83`, `74`, `61`, `45`, `-5`
 """
+def getGrade(score):
+    if score < 0 or score > 100:
+        return "Invalid score"
+    elif score >= 90:
+        return "A"
+    elif score >= 80:
+        return "B"
+    elif score >= 70:
+        return "C"
+    elif score >= 60:
+        return "D"
+    else:
+        return "F"
+
+def printReport(name, score):
+    grade = getGrade(score)
+    print(f"{name} scored {score}/100 and received a {grade}")
+
+# tests
+printReport("Manali", 85)
+printReport("John", 105)
+printReport("Jane", 95)
+printReport("Benny", 83)
+printReport("Basab", 74)
+printReport("Cyrus", 61)
+printReport("Mateo", 45)
+printReport("Utkarsh", -5)
+printReport("Boundary Test", 79)
